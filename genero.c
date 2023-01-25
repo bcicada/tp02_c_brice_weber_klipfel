6d535689fb1ed18ea2fcc88a810ec4212c7d135b
Brice WEBER KLIPFEL <b.cicada@protonmail.com>
2023-01-25 09:07:34 +0000

    Init

---
+++ genero.c
@@ -0,0 +1,9 @@
+/**
+ * \file genero.c
+ * \brief System & process calls.
+ * \author Brice W. K. <b.cicada@protonmail.com>
+ * \version 0.1
+ * \date 22 sept. 2022
+ *
+ * System & process calls in c file.
+ */
