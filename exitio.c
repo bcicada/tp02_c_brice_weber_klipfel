2db3691f8e8ad0a358216118ecd9016891d49899
Brice WEBER KLIPFEL <b.cicada@protonmail.com>
2023-01-25 10:16:12 +0000

    Add exitio

--- exitio.c
+++ exitio.c
@@ -10,6 +10,9 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
@@ -18,6 +21,27 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    printf("Message: %s\n", argv[1]);
+    pid_t pid;
+    int status;
+    int fd;
+
+    pid = getpid();
+    printf("PID du père: %d\n", pid);
+
+    pid = fork();
+    if (pid == 0) {
+        // Processus fils
+        printf("PID du fils: %d\n", getpid());
+        close(2); // Ferme le descripteur 2 (stderr)
+        fd = mkstemp("/tmp/proc-exercise");
+        dup2(fd, 1);
+        printf("Numéro de descripteur du fichier: %d\n", fd);
+        execlp(argv[1], argv[1], NULL);
+    } else {
+        // Processus père
+        wait(&status);
+        printf("That's All Folks!\n");
+    }
+
     return 0;
 }
